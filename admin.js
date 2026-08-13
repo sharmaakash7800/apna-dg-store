b646592999f40e64bea57df37ae68afd4c8ccfaf
Akash Sharma <sharmaakash7800@gmail.com>
2026-08-13 06:01:57 +0530

    Connect latest Google Sheet Web App URL with auto-deletion support AKfycbyPS389...

--- admin.js
+++ admin.js
@@ -6,7 +6,7 @@ let productsList = [], suppliersList = [], poCart = [], editPoCart = [], tickedP
 let editingProductIds = new Set(), lastEditExitTimestamp = 0;
 
 /* GOOGLE SHEETS AUTOMATIC SYNC LOGIC */
-const DEFAULT_SHEET_URL = "https://script.google.com/macros/s/AKfycbzDmR4Jn2WsxQNMPyUltcPpSbZwCbG1AsTV-hkxKBAKWX5IUmC4ySAvj4KQ16z2JCTaVA/exec";
+const DEFAULT_SHEET_URL = "https://script.google.com/macros/s/AKfycbyPS3892GpqKPPoL3gkHLK2BnMtXrW2j9ALCBqblf82uLi9rslEVh2eaGOMwi9UT6-R7Q/exec";
 let googleSheetScriptUrl = DEFAULT_SHEET_URL;
 localStorage.setItem("googleSheetScriptUrl", DEFAULT_SHEET_URL);
 
